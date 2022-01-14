3f38cf672c9d2c1d81bb4e17411fb042818c57e2
Baptiste <74823118+baptisteCanac@users.noreply.github.com>
2022-01-14 19:04:32 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+function openSignUp(){
+	window.open("signup.html", "_self");
+}
+function openSignIn(){
+	window.open("login.html", "_self");
+}
+function goBack(){
+	window.open("login.html", "_self");
+}
